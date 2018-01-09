d900f6c158b3d34f5586c0d1039b28402fa32dc7
Vinicius Braz Pereira <vinicius@jaya.tech>
2018-01-09 19:39:47 -0200

    Remove GreetingController

--- src/test/kotlin/com/wishlister/controller/HomeControllerTest.kt
+++ src/test/kotlin/com/wishlister/controller/HomeControllerTest.kt
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 /* @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class GreetingControllerTest {
+class HomeControllerTest {
 	
 	@Autowired
     lateinit var restTemplate: TestRestTemplate
